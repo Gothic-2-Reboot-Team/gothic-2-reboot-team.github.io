cec522aa6386507b45c8dbcdbfd741c1299419fc
doicrm <michalakradek2@gmail.com>
2024-10-18 23:08:38 +0200

    Integration with Vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,24 @@ import '@/assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from '@/App.vue'
 import router from '@/router'
-import i18n from '@/i18n'
+import i18n from '@/config/i18n.config'
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 app.use(i18n)
 app.mount('#app')
