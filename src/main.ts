abf9b293d78225821d1b538773f1317d03ceac3d
doicrm <michalakradek2@gmail.com>
2024-10-18 23:16:28 +0200

    Added mdi font

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import '@/assets/main.css'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
-import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
